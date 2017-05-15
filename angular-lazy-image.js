7cf3829f25caa11a494f1eb1d718067ec7d29ad4
林志雄 <linzhixiong@enn.cn>
2017-05-15 16:48:31 +0800

    watch the attrs changes

--- angular-lazy-image.js
+++ angular-lazy-image.js
@@ -2,52 +2,60 @@
     angular.module('ngLazyImage', []).directive('lazyImage', [function () {
         return {
             link: function (scope, element, attrs) {
-                var thumbnail = $('<img>', {
-                    'src': attrs.thumbnail,
-                    'class': 'thumbnail',
-                    width: attrs.width,
-                    height: attrs.height
-                });
+                scope.$watch(function () {
+                    return [attrs.width, attrs.height, attrs.original];
+                }, function () {
+                    lazyLoad();
+                }, true);
 
-                thumbnail.appendTo(element);
+                function lazyLoad() {
+                    element.empty();
+                    var thumbnail = $('<img>', {
+                        'src': attrs.thumbnail,
+                        'class': 'thumbnail',
+                        width: attrs.width,
+                        height: attrs.height
+                    });
 
-                if (isScrolledIntoView(element)) {
-                    loadOriginalImage()
-                }
+                    thumbnail.appendTo(element);
 
-                var timer;
-                $(element.parent()).on('scroll', function (e) {
-                    //debounce the scroll 
-                    if (timer) {
-                        clearTimeout(timer);
+                    if (isScrolledIntoView(element)) {
+                        loadOriginalImage()
                     }
-                    timer = setTimeout(function () {
-                        if (isScrolledIntoView(element)) {
-                            loadOriginalImage()
+
+                    var timer; //debounce the scroll
+                    $(element.parent()).on('scroll', function (e) {
+                        if (timer) {
+                            clearTimeout(timer);
                         }
-                    }, 50);
-                });
-
-                function isScrolledIntoView(elem) {
-                    var docViewTop = $(elem).scrollTop();
-                    var docViewBottom = docViewTop + $(elem.parent()).height();
-
-                    var elemTop = $(elem).offset().top - $(elem.parent()).offset().top;
-                    var elemBottom = elemTop + $(elem).height();
-                    return ((elemTop <= docViewBottom) && (elemBottom >= docViewTop));
-                }
-
-                function loadOriginalImage() {
-                    var original = $('<img>', {
-                        'src': attrs.original
+                        timer = setTimeout(function () {
+                            if (isScrolledIntoView(element)) {
+                                loadOriginalImage()
+                            }
+                        }, 50);
                     });
 
-                    original.hide();
-                    original.appendTo(element);
-                    original.on('load', function (event) {
-                        thumbnail.attr({src: attrs.original});
-                        thumbnail.attr({'class': 'original'});
-                    });
+                    function isScrolledIntoView(elem) {
+                        var docViewTop = $(elem).scrollTop();
+                        var docViewBottom = docViewTop + $(elem.parent()).height();
+
+                        var elemTop = $(elem).offset().top - $(elem.parent()).offset().top;
+                        var elemBottom = elemTop + $(elem).height();
+                        return ((elemTop <= docViewBottom) && (elemBottom >= docViewTop));
+                    }
+
+                    function loadOriginalImage() {
+                        var original = $('<img>', {
+                            'src': attrs.original
+                        });
+
+                        original.hide();
+                        original.appendTo(element);
+                        original.on('load', function (event) {
+                            thumbnail.attr({src: attrs.original});
+                            thumbnail.attr({'class': 'original'});
+                        });
+                    }
                 }
             }
         }
